6f8b3e8dc60341ef3c045095d3b36c483ad21a90
marre421 <marcus.nordin421@hotmail.com>
2020-10-06 17:17:44 +0200

    Testing doctrine

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Entity\User;
+use App\Repository\ProductRepository;
 use App\Service\ResponseService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
-    public function index(ResponseService $responseService): Response
+    public function index(ResponseService $responseService, ProductRepository $productRepository): Response
     {
         $request = Request::createFromGlobals();
 
+        $manager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setName('test');
+        $manager->persist($user);
+        $manager->flush();
+
+        $product = new Product();
+        $product->setTitle('Test');
+        $product->setUser($user);
+        $manager->persist($product);
+        $manager->flush();
+
         return $responseService->json([
-            'request' => $request->query->get('hello')
+            'request' => $request->query->get('hello'),
+            'products' => $productRepository->findAll()
         ]);
     }
 }
